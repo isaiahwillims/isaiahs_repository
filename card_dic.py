8c97cefb120e60a3e94feb530cd77b204488e521
johnjohnchase <llcooljohn@gmail.com>
2014-02-05 11:41:08 -0700

    added cards file and script.

---
+++ card_dic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+magic_cards = {'Witchstalker':{'Cost':'1GG', 'Type':'Creature — Wolf', 'Pow/Tgh':'(3/3)', 
+                               'Rules Text':'''Hexproof (This creature can't be the target of spells or abilities your opponents control.)
+Whenever an opponent casts a blue or black spell during your turn, put a +1/+1 counter on Witchstalker.''',
+                                'Set/Rarity': 'Magic 2014 Core Set Rare'}} 
+
+
+
+print magic_cards['Witchstalker']['Cost']
+
+# Name	Witchstalker
+# Cost:	 1GG
+# Type:	 Creature — Wolf
+# Pow/Tgh:	 (3/3)
+# Rules Text:	 Hexproof (This creature can't be the target of spells or abilities your opponents control.)
+# Whenever an opponent casts a blue or black spell during your turn, put a +1/+1 counter on Witchstalker.
+# Set/Rarity:	 Magic 2014 Core Set Rare
+
+
+
+
+
+
+
+
+
+
+# magic_cards['Witchstalker']['Type']
+# 'Creature — Wolf'
+
