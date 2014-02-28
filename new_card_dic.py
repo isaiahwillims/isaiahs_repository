8873b5e3de64728b3d8c27cf35a655602bb130f7
isaiahwilliams <stardustdragon258@gmail.com>
2014-02-28 10:39:19 -0700

    removed useless lines

--- new_card_dic.py
+++ new_card_dic.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# magic_cards = {'Witchstalker':{'Cost':'1GG', 'Type':'Creature — Wolf', 'Pow/Tgh':'(3/3)', 
-#                                'Rules Text':'''Hexproof (This creature can't be the target of spells or abilities your opponents control.)
-# Whenever an opponent casts a blue or black spell during your turn, put a +1/+1 counter on Witchstalker.''',
-#                                 'Set/Rarity': 'Magic 2014 Core Set Rare'},
-#                 'Woodborn Behemoth': } 
-
-#
 cardname = {}
 cardstat = {}
 
